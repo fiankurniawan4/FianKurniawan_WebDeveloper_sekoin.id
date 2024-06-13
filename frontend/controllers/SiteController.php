15b8f21210fdc8aa14c3809e0fb7a1884297911f
fiankurniawan4 <162801177+fiankurniawan4@users.noreply.github.com>
2024-06-13 11:38:30 +0700

    Add files via upload

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -4,6 +4,7 @@ namespace frontend\controllers;
 
 use yii\web\Controller;
 use common\models\Produk;
+use Yii;
 
 /**
  * Site controller
@@ -28,6 +29,19 @@ class SiteController extends Controller
             'produk' => $produk,
             'pagination' => $pagination
         ]);
+    }
+
+    // error page
+    public function actionError()
+    {
+        $exception = Yii::$app->errorHandler->exception;
+        if ($exception !== null) {
+            return $this->render('error', [
+                'name' => $exception->getMessage(),
+                'message' => $exception->getMessage(),
+                'exception' => $exception
+            ]);
+        }
     }
 
 }
